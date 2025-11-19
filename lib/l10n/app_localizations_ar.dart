7b5dd403f70e9d32f67ae746e5efe0e38e008dee
yousefsultan <yousefsultan280@gmail.com>
2025-11-19 17:03:29 +0200

    first part(login&signup)

---
+++ lib/l10n/app_localizations_ar.dart
@@ -0,0 +1,26 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get lookGoodFeelGood => 'مظهر رائع,شعور رائع';
+
+  @override
+  String get createYourIndividualAndUniqueStyle =>
+      'أنشئ اسلوبك الفردي والفريد لتبدو مذهل كل يوم.';
+
+  @override
+  String get men => 'رجال';
+
+  @override
+  String get women => 'نساء';
+
+  @override
+  String get skip => 'تخطي';
+}
---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,26 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get lookGoodFeelGood => 'Look Good,Feel Good';
+
+  @override
+  String get createYourIndividualAndUniqueStyle =>
+      'Create your individual & unique style and look amazing everyday.';
+
+  @override
+  String get men => 'Men';
+
+  @override
+  String get women => 'Women';
+
+  @override
+  String get skip => 'Skip';
+}
